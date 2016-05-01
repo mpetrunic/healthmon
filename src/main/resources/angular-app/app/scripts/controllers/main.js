c725ec06c0f2940bef28a706f31fdc4276526328
MarinPetrunic <marin.petrunic@gmail.com>
2016-05-01 22:03:22 +0200

    Removed angular template page with yeoman generated one

---
+++ src/main/resources/angular-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name healthmonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the healthmonApp
+ */
+angular.module('healthmonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
