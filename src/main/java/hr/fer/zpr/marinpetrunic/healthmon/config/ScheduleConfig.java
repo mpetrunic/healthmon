0890b65a2c3bfed6b7affd1e22f6cf039c9c65df
MarinPetrunic <marin.petrunic@gmail.com>
2016-05-29 17:22:42 +0200

    Scheduling configured

---
+++ src/main/java/hr/fer/zpr/marinpetrunic/healthmon/config/ScheduleConfig.java
@@ -0,0 +1,14 @@
+package hr.fer.zpr.marinpetrunic.healthmon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author MarinPetrunic
+ */
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class ScheduleConfig {
+}
